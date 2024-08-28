6306fbe7f0475c525ea6085e5330bd7d023c2953
emmanuella <emmavduru@gmail.com>
2024-08-28 20:54:24 +0100

    add cron job

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, render_template
+from flask_apscheduler import APScheduler
 
 from config import config
 from helpers.functions import get_articles, get_country_info
+from db import connect_to_db
 
 app = Flask(__name__)
+scheduler = APScheduler()
 
 @app.route("/countries", methods=["GET"])
 def home():
@@ -20,5 +23,16 @@ def get_map():
 	return render_template("home.html")
 
 
+# Cron job to delete all rows from the News table at midnight
+@scheduler.task("cron", id="clear_news_table", hour=0, minute=0)
+def clear_news_table():
+	cur, conn = connect_to_db()
+	cur.execute("DELETE FROM News;")
+	cur.close()
+	print("News Table cleared.")
+	
+
 if __name__ == "__main__":
-	app.run(debug=config["debug"])
+	scheduler.init_app(app)
+	scheduler.start()
+	app.run(debug=False)
--- scripts/clear_db.py
+++
@@ -1,6 +0,0 @@
-from ..db import connect_to_db
-
-# Remove all rows from the news table.
-cur, conn = connect_to_db()
-cur.execute("DELETE FROM News;")
-cur.close()
